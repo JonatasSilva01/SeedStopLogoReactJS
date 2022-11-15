5f73549b2da6ba8f48ea369365e3d3754f8818a8
JonatasSilva01 <jhownsantossilva@gmail.com>
2022-11-15 16:15:23 -0300

    update speed runing with react js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ function App() {
   const [speed, setSpeed] = useState(false);
   const speedClass = speed ? 'speed' : 'Stop';
 
+  const [counter, setConter] = useState(0);
+
   const [btnStopColor, setBtnStopColor] = useState(false);
   const btnStopColorClass = btnStopColor ? 'StopColor' : ''
 
   const hendless = () => {
     setSpeed(!speed);
     setBtnStopColor(!btnStopColor);
+    setConter(counter + 1);
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className={`App-logo ${speedClass}`} alt="logo" />
+        {counter <= 0 ? null :  <h1>Apertei botão {counter} vezes </h1>}
         <button className={`${btnStopColorClass}`} type='button' onClick={hendless}>{speedClass}</button>
       </header>
     </div>
