0b9e470b421a66fe7208b22edae0bd102828d8de
JonatasSilva01 <jhownsantossilva@gmail.com>
2022-11-15 16:06:42 -0300

    speed stop with react js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [speed, setSpeed] = useState(false);
+  const speedClass = speed ? 'speed' : 'Stop';
+
+  const [btnStopColor, setBtnStopColor] = useState(false);
+  const btnStopColorClass = btnStopColor ? 'StopColor' : ''
+
+  const hendless = () => {
+    setSpeed(!speed);
+    setBtnStopColor(!btnStopColor);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className={`App-logo ${speedClass}`} alt="logo" />
+        <button className={`${btnStopColorClass}`} type='button' onClick={hendless}>{speedClass}</button>
       </header>
     </div>
   );
